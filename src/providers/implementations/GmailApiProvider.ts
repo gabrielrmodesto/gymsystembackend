c7068fe8110b824fca22776ab3c586e29986469f
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-11-26 00:38:07 -0300

    Gmail api started

---
+++ src/providers/implementations/GmailApiProvider.ts
@@ -0,0 +1,29 @@
+import nodemailer from 'nodemailer';
+import Mail from 'nodemailer/lib/mailer';
+import { IGmailAPIProvider, IMessage } from '../IGmailAPIProvider';
+
+export class GmailApiProvider implements IGmailAPIProvider{
+	private transporter: Mail;
+
+	constructor(){
+		this.transporter = nodemailer.createTransport({
+
+		})
+	}
+
+	async sendMail(message: IMessage): Promise<void> {
+		await this.transporter.sendMail({
+			to: {
+				name: message.to.name,
+				address: message.to.email,
+			},
+			from: {
+				name: message.from.name,
+				address: message.from.email,
+			},
+			subject: message.subject,
+			html: message.body
+		})
+	}
+
+}
